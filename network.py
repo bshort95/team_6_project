7afccccfdaf1fc28ea1f1e3a758981139f3a194e
Brandon Short <71357028+bshort95@users.noreply.github.com>
2020-12-09 19:48:56 -0700

    Update network.py

--- network.py
+++ network.py
@@ -6,7 +6,7 @@ import socket
 class Network:
     def __init__ (self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "10.49.58.140"
+        self.server = "" # add your ip number here
         self.port = 5555
         self.addr = (self.server, self.port)
         self.pos = self.connect()
@@ -29,4 +29,3 @@ class Network:
             print(e)
 
 
-#n = Network()
